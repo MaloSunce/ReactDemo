11cf0479db46f0bb9261968c033d7136c60ea448
Sara <sarasdj@stud.ntnu.no>
2025-01-14 18:31:24 +0100

    update: header layout

--- ReactDemo/src/header-footer/Header.jsx
+++ ReactDemo/src/header-footer/Header.jsx
@@ -1,16 +1,29 @@
 import "./Header.css"
+import GitHubLogo from '../assets/github.png'
+import LinkedInLogo from '../assets/linkedin.png'
+import SunLogo from '../assets/sun.png'
 
 function Header() {
+
     return (
         <header className="Header">
             <h1><a href="#">MaloSunce</a></h1>
             <nav className="Nav">
-                <a href="#">Home</a>
+                <a href="#">About me</a>
                 <a href="#">Skills</a>
-                <a href="#">About</a>
+                <a href="#">Projects</a>
+                <a href="#">Classes</a>
                 <a href="#">Contact</a>
-                <a href="#">LinkedIn</a>
+
+                <img className="LinkLogo" src={GitHubLogo} alt="GitHub logo"></img>
+                {/*<dir className="Logo">
+                <img src="../assets/sun.png" alt="Sun Icon"> </img>
+            </dir>*/}
+                <img className="LinkLogo" src={LinkedInLogo} alt="LinkedIn logo"></img>
+                {/*<a href="https://www.flaticon.com/free-icons/brightness" title="brightness icons">Brightness icons created
+                by Cap Cool - Flaticon</a>*/}
             </nav>
+            <img className="Logo" src={SunLogo} alt="Sun logo"></img>
         </header>
     );
 }
