947e91f857163db02945cc7bbfaaf102768f5a23
Sara <sarasdj@stud.ntnu.no>
2025-01-14 11:25:41 +0100

    add: Header.jsx

---
+++ ReactDemo/src/Header.jsx
@@ -0,0 +1,21 @@
+function Header() {
+
+    return (
+        <header>
+            <h1>
+                Hello World!
+                <nav>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </nav>
+                <hr></hr>
+            </h1>
+        </header>
+    );
+}
+
+export default Header
