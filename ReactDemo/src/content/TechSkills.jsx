f36dfd70d9bf05cfc6d83d1d5613943c91b7ed3e
Sara <sarasdj@stud.ntnu.no>
2025-01-17 09:31:35 +0100

    update: TechSkills table design

--- ReactDemo/src/content/TechSkills.jsx
+++ ReactDemo/src/content/TechSkills.jsx
@@ -4,22 +4,23 @@ import * as React from 'react';
 
 function TechSkills() {
 
+    // [skill, proficiency]
     const backend = [
-        ["Kotlin", 0.8],
-        ["C/C++", 0.7],
-        ["Golang", 0.8],
-        ["Python", 0.7],
-        ["Dart", 0.6],
-        ["SQL", 0.6]
+        ["Kotlin", 5],
+        ["C/C++", 4],
+        ["Golang", 5],
+        ["Python", 5],
+        ["Dart", 4],
+        ["SQL", 3]
     ];
 
     const frontend = [
-        ["React", 0.5],
-        ["JavaScript", 0.4],
-        ["HTML", 0.6],
-        ["CSS", 0.7],
-        ["Jetpack Compose", 0.8],
-        ["Flutter", 0.8]
+        ["React", 2],
+        ["JavaScript", 3],
+        ["HTML", 3],
+        ["CSS", 3],
+        ["Jetpack Compose", 5],
+        ["Flutter", 4]
     ];
 
     const other = ["Git", "GCP", "MongoDB", "Linux", "Windows", "Android"];
@@ -38,7 +39,6 @@ function TechSkills() {
                 </thead>
                 <tbody>
                 {/* Map each (skill, proficiency) pair to a row in each their own column */}
-                {Math.max(backend.length, frontend.length, other.length)}
                 {Array(Math.max(backend.length, frontend.length, other.length))
                     .fill(null)
                     .map((_, index) => (
@@ -47,30 +47,29 @@ function TechSkills() {
                                 {backend[index] ? (
                                     <div className="SkillItem">
                                         <div>{backend[index][0]}</div>
-                                        <div style={{backgroundColor: 'var(--accent)', width: `${backend[index][1] * 100}%`}}></div>
+                                        <div style={{backgroundColor: 'var(--accent)', width: `${backend[index][1] * 18}%`, borderRadius: '10px'}}></div>
                                     </div>
-                                ) : (
-                                    <div></div> //Empty div is default for missing data
-                                )}
+                                ) :
+                                    <div></div> // Empty div as default for missing data
+                                }
                             </td>
 
                             <td>
                                 {frontend[index] ? (
                                     <div className="SkillItem">
                                         <div>{frontend[index][0]}</div>
-                                        <div style={{backgroundColor: 'var(--accent)', width: `${frontend[index][1] * 100}%`}}></div>
+                                        <div style={{backgroundColor: 'var(--accent)', width: `${frontend[index][1] * 18}%`, borderRadius: '10px'}}></div>
                                     </div>
-                                ) : (
-                                    <div></div>
-                                )}
+                                ) :
+                                    <div></div> // Empty div as default for missing data
+                                }
                             </td>
 
-                            <td>
-                                {other[index] ? other[index] : ""}
-                            </td>
+                            <td> {other[index] ? other[index] : ""} </td>
                         </tr>
                     ))}
                 </tbody>
+
             </table>
         </div>
     );
