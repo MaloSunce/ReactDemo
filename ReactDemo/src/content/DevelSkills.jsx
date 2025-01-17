4c40752996b052c4ef616097a29aed42e3ea7a0d
Sara <sarasdj@stud.ntnu.no>
2025-01-17 12:33:39 +0100

    add: initial DevelSkills content

--- ReactDemo/src/content/DevelSkills.jsx
+++ ReactDemo/src/content/DevelSkills.jsx
@@ -1,8 +1,28 @@
 import './DevelSkills.css'
+
 function DevelSkills() {
-    return(
+    return (
         <div className="DevelSkills" id="DevelSkills">
-            <h2 className="Title">Software & Design Expertise</h2>
+            <h2>Software & Design Expertise</h2>
+            <b style={{fontSize: '1.4em'}}>Placeholder text placeholder text</b>
+            <div style={{display: 'flex', flexDirection: 'row', margin: '0 auto', paddingTop: '2%'}}>
+                <ul style={{textAlign: 'left'}}>
+                    <li>Mobile programming</li>
+                    <li>Web development and REST-applications</li>
+                    <li>Cross-platform development</li>
+                    <li>User centered design</li>
+                    <li>Cloud technologies</li>
+                    <li>API integration</li>
+                </ul>
+                <ul style={{textAlign: 'right'}}>
+                    <li>Agile development methods</li>
+                    <li>Software security</li>
+                    <li>Documentation and testing</li>
+                    <li>Data modeling and database systems</li>
+                    <li>Containerization and virtualization</li>
+                    <li>Object-oriented programming</li>
+                </ul>
+            </div>
         </div>
     );
 }
