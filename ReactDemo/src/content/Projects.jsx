6e5ec63680b47ad0ad972a5ae2d98a4e1cdcc196
Sara <sarasdj@stud.ntnu.no>
2025-01-20 17:00:43 +0100

    update: Projects slider responsive num o slides

--- ReactDemo/src/content/Projects.jsx
+++ ReactDemo/src/content/Projects.jsx
@@ -41,28 +41,21 @@ function Projects() {
         slidesToScroll: 1,
         responsive: [
             {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 600,
+                breakpoint: 1000,
                 settings: {
                     slidesToShow: 2,
                     slidesToScroll: 2
                 }
             },
             {
-                breakpoint: 480,
+                breakpoint: 600,
                 settings: {
                     slidesToShow: 1,
-                    slidesToScroll: 1
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
                 }
-            }
+            },
         ],
     };
 
