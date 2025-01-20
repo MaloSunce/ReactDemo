218a8bc8b05e01debac71aea562845ad719896f9
Sara <sarasdj@stud.ntnu.no>
2025-01-20 14:10:36 +0100

    update: remove git and linkedin icon from floatingheader

--- ReactDemo/src/header-footer/FloatingHeader.jsx
+++ ReactDemo/src/header-footer/FloatingHeader.jsx
@@ -76,17 +76,6 @@ function FloatingHeader() {
                 <a href="#Projects">Projects</a>
                 <a href="#Footer">Contact</a>
 
-                <div style={{display: 'flex', gap: '1.7em'}}>
-                    <a href="https://github.com/MaloSunce?tab=repositories" target="_blank">
-                        <img className="Icons" src={GitHubLogo} alt="GitHub logo"></img>
-                        {/*<dir className="Logo"><img src="../assets/sun.png" alt="Sun Icon"> </img></dir>*/}
-                    </a>
-                    <a href="https://www.linkedin.com/in/sara-djordjevic-2b0ab925b/" target="_blank">
-                        <img className="Icons" src={LinkedInLogo} alt="LinkedIn logo"></img>
-                        {/*<a href="https://www.flaticon.com/free-icons/brightness" title="brightness icons">Brightness icons created by Cap Cool - Flaticon</a>*/}
-                    </a>
-                    {/*<a id="color-switch"><FontAwesomeIcon icon="palette"/></a>*/}
-                </div>
                 <button onClick={showNavbar} className="NavBtn NavBtnClose"><FontAwesomeIcon icon="close"/></button>
             </nav>
             <button onClick={showNavbar} className="NavBtn"><FontAwesomeIcon icon="bars"/></button>
